ac64c0157f3ca74101b68cb70bfe42cd7b5cc687
soundrapandian10 <s.soundrapandian10@gmail.com>
2024-09-15 18:00:24 +0530

    Added Sql

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -1,9 +1,10 @@
 package guru.springframework.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-
 import guru.springframework.domain.Category;
+import java.util.Optional;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
@@ -1,9 +1,10 @@
 package guru.springframework.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-
 import guru.springframework.domain.UnitOfMeasure;
+import java.util.Optional;
 
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
 
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
