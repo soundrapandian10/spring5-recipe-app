4cd04a70dd6a5acb6fda1e00c37e89a711756141
soundrapandian10 <s.soundrapandian10@gmail.com>
2025-02-11 23:18:40 +0530

    - Added Option to add ingredients

--- src/main/java/guru/springframework/domain/Category.java
+++ src/main/java/guru/springframework/domain/Category.java
@@ -1,13 +1,15 @@
 package guru.springframework.domain;
 
-import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Set;
 
-@Data
-@EqualsAndHashCode(exclude = { "recipes" })
+@Getter
+@Setter
+@EqualsAndHashCode(exclude = {"recipes"})
 @Entity
 public class Category {
 
